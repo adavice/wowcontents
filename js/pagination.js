482ffd7e8d12463a68e9ce78214c39d2da223dc8
adavice <97439259+adavice@users.noreply.github.com>
2022-05-26 13:43:37 +0300

    made redesign of web-page

---
+++ js/pagination.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    $('.for_step_1').on('click', function () {
+        $('.gallery-grids').addClass('no_active_block');
+        $('.galery_link').removeClass('active');
+
+        $('.step-1').removeClass('no_active_block').addClass('active_block');
+        $('.for_step_1').addClass('active');
+    });
+
+    $('.for_step_2').on('click', function () {
+        $('.gallery-grids').addClass('no_active_block');
+        $('.galery_link').removeClass('active');
+
+        $('.step-2').removeClass('no_active_block').addClass('active_block');
+        $('.for_step_2').addClass('active');
+    });
+
+    $('.for_step_3').on('click', function () {
+        $('.gallery-grids').addClass('no_active_block');
+        $('.galery_link').removeClass('active');
+
+        $('.step-3').removeClass('no_active_block').addClass('active_block');
+        $('.for_step_3').addClass('active');
+    });
+
+    $('.for_step_4').on('click', function () {
+        $('.gallery-grids').addClass('no_active_block');
+        $('.galery_link').removeClass('active');
+
+        $('.step-4').removeClass('no_active_block').addClass('active_block');
+        $('.for_step_4').addClass('active');
+    });
+
+    $('.for_step_5').on('click', function () {
+        $('.gallery-grids').addClass('no_active_block');
+        $('.galery_link').removeClass('active');
+
+        $('.step-5').removeClass('no_active_block').addClass('active_block');
+        $('.for_step_5').addClass('active');
+    });
+})
